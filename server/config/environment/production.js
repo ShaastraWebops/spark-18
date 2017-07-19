4b6dac6269fe76d7c7549844ff27e5c8a46da15e
Gokulan <gokulan97@gmail.com>
2017-07-19 09:37:33 +0530

    MongoDB auth added

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -19,6 +19,6 @@ module.exports = {
     uri: process.env.MONGODB_URI
       || process.env.MONGOHQ_URL
       || process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_APP_NAME
-      || 'mongodb://localhost/summitregistations2018'
+      || 'mongodb://'+process.env.SPARK_USER+":"+process.env.SPARK_PASSWORD+"@localhost/spark";
   }
 };
