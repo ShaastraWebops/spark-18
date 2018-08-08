66d595d8a1f9a2df8e662f3cc8b2aff08da88e65
Nilay Gautam <nilaygautam2007@gmail.com>
2018-08-08 16:10:46 +0530

    Added landing page

--- server/config/socketio.js
+++ server/config/socketio.js
@@ -18,7 +18,6 @@ function onConnect(socket) {
   // Insert sockets below
   require('../api/city/city.socket').register(socket);
   require('../api/participant/participant.socket').register(socket);
-  require('../api/thing/thing.socket').register(socket);
 }
 
 export default function(socketio) {
